82db5fbbf53096db074bee99bd3be0f93a8c55f9
Antoine <aeroflash@hotmail.fr>
2017-04-12 23:32:46 +0200

    FIRST STEP MAAAAAAN
    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil WAMPSERVER</title>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+	<link id="stylecall" rel="stylesheet" href="wampthemes/classic/style.css" />
+	<link rel="shortcut icon" href="index.php?img=favicon" type="image/ico" />
+</head>
+
+<body>
+	<p>gfljz"aeiuyhfaeithueormjsvgmkildjisgp</p>
+</body>
+</html>
