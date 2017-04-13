a8d01b4a2600052a1ef7bf9ef9eb8722b43271ae
Antoine <aeroflash@hotmail.fr>
2017-04-13 15:39:20 +0200

    suite des modifs

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -3,9 +3,9 @@
 	<head>
 		<title>Accueil</title>
 		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  		<link type="text/css" rel="stylesheet" href="http://savu.axelbrouland.fr/assets/css/materialize.min.css">
-  		<link type="text/css" rel="stylesheet" href="http://savu.axelbrouland.fr/assets/css/materialize.css">
-  		<link type="text/css" rel="stylesheet" href="http://savu.axelbrouland.fr/assets/css/style.css">
+  		<link type="text/css" rel="stylesheet" href=$config['base_url']+"/assets/css/materialize.min.css">
+  		<link type="text/css" rel="stylesheet" href=$config['base_url']+"/assets/css/materialize.css">
+  		<link type="text/css" rel="stylesheet" href=$config['base_url']+"/assets/css/style.css">
       	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       	<style>
       		#lol{
@@ -24,11 +24,11 @@
 		<?php include($menu); ?>
 		<section>
 			<div id="lolol">
-				<img id="lol" src="http://savu.axelbrouland.fr/assets/LOGO.png">
+				<img id="lol" src=$config['base_url']>
 			</div>
 		</section>
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-		<script type="text/javascript" src="http://savu.axelbrouland.fr/assets/js/materialize.js"></script>
-		<script type="text/javascript" src="http://savu.axelbrouland.fr/assets/js/materialize.min.js"></script>
+		<script type="text/javascript" src=$config['base_url']+"/assets/js/materialize.js"></script>
+		<script type="text/javascript" src=$config['base_url']+"/assets/js/materialize.min.js"></script>
 	</body>
 </html>
